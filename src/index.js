d1938924a696dda78dca74fa550066c68ea9df1c
Viktor <imjustsnoopy@gmail.com>
2021-03-13 23:25:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        stack.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    for (let i = 0; i < stack.length; i++) {
+        if (str.includes(stack[i])) {
+            str = str.replace(stack[i], "");
+            i = -1;
+            console.log(str);
+        }
+    }
+
+    if (str.length % 2 != 0) {
+        return false;
+    }
+
+    if (str.length != 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
